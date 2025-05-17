ce511a488aa1f0ae6fe6bcc7138a2f54b766bf6c
Fiqo Anugrah <fiqoanugrah@Fiqos-MacBook.local>
2025-05-18 03:32:04 +1000

    Fix WSGI configuration

--- tabletap/wsgi.py
+++ tabletap/wsgi.py
@@ -1,7 +1,18 @@
+# tabletap/wsgi.py
 import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
+
+# Tambahkan path proyek ke Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabletap.settings')
 
+# Simple application
 application = get_wsgi_application()
-app = application  # Needed for Vercel
+
+# Untuk Vercel
+app = application
