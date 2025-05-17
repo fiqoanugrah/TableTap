06b65afa6f72750514400bcbb819c0c958309816
Fiqo Anugrah <fiqoanugrah@Fiqos-MacBook.local>
2025-05-18 03:37:51 +1000

    Revert DEBUG to False for production

--- tabletap/settings.py
+++ tabletap/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '.vercel.app']
 VERCEL_DEPLOYMENT = os.environ.get('VERCEL_DEPLOYMENT', 'False') == 'True'
