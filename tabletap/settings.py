08f76183da4477fa8991ad7a724742d47a43874b
Fiqo Anugrah <fiqoanugrah@Fiqos-MacBook.local>
2025-05-18 03:31:19 +1000

    Temporarily enable debug for error identification

--- tabletap/settings.py
+++ tabletap/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 VERCEL_DEPLOYMENT = os.environ.get('VERCEL_DEPLOYMENT', 'False') == 'True'
 
 # Application definition
