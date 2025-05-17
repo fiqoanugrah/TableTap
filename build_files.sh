e72867c30e7a1c5ef3d9661e48bfbcc7b54c1fa7
Fiqo Anugrah <fiqoanugrah@Fiqos-MacBook.local>
2025-05-18 03:14:33 +1000

    Fix build script for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # build_files.sh
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+# Install Python dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
